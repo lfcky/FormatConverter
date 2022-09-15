703df11ac9a6901ac1d70cb0fcb54202f1122d04
leaf <41480467+lfcky@users.noreply.github.com>
2022-09-16 00:14:22 +0800

    json转换为多sheet的excel文件

--- jsonToExcel.py
+++ jsonToExcel.py
@@ -4,8 +4,11 @@ import pandas as pd
 
 file_list = fu.read_json()
 
+writer = pd.ExcelWriter(fu.out_excel() + '/' + 'result.xlsx')
 for file in file_list:
     with open(file, 'r', encoding='utf-8') as f:
         data = json.load(f)
     data = pd.DataFrame(data)
-    data.to_excel(fu.out_excel() + '/' + fu.get_file_name(file) + '.xlsx', index=None)
+    data.to_excel(writer, fu.get_file_name(file), index=None)
+writer.save()
+writer.close()
